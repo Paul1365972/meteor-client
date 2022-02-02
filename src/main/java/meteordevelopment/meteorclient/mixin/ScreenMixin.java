41dd218f815dd344d1971d2d510f1500db6af79f
paul1365972 <paul1365972@gmail.com>
2022-02-02 20:24:22 +0100

    Fix mixin warning in ScreenMixin

--- src/main/java/meteordevelopment/meteorclient/mixin/ScreenMixin.java
+++ src/main/java/meteordevelopment/meteorclient/mixin/ScreenMixin.java
@@ -8,17 +8,11 @@ package meteordevelopment.meteorclient.mixin;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import meteordevelopment.meteorclient.systems.modules.render.NoRender;
 import meteordevelopment.meteorclient.utils.Utils;
-import meteordevelopment.meteorclient.utils.tooltip.MeteorTooltipData;
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.tooltip.TooltipComponent;
-import net.minecraft.client.item.TooltipData;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
-
-import java.util.List;
 
 @Mixin(Screen.class)
 public abstract class ScreenMixin {
@@ -26,13 +20,5 @@ public abstract class ScreenMixin {
     private void onRenderBackground(CallbackInfo info) {
         if (Utils.canUpdate() && Modules.get().get(NoRender.class).noGuiBackground())
             info.cancel();
-    }
-
-    @Inject(method = "method_32635", at = @At("HEAD"), cancellable = true, locals = LocalCapture.CAPTURE_FAILHARD)
-    private static void onComponentConstruct(List<TooltipComponent> list, TooltipData data, CallbackInfo info) {
-        if (data instanceof MeteorTooltipData) {
-            list.add(((MeteorTooltipData) data).getComponent());
-            info.cancel();
-        }
     }
 }
